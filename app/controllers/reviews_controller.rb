22e2b464e6c922437f7346cd1d71fe8acc68f55b
melvinadu <contactmelvinadu@gmail.com>
2021-02-17 18:39:15 -0500

    added filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,21 @@ class ReviewsController < ApplicationController
   def index
     # this is our list page for our reviews
 
-    @number = rand(100)
+    @price = params[:price]
+    @cuisine = params[:cuisine]
 
-
+    # start with all the reviews
     @reviews = Review.all
+
+    # filtering by price
+    if @price.present?
+      @reviews = @reviews.where(price: @price)
+    end
+
+    # filtering by cuisine
+    if @cuisine.present?
+      @reviews = @reviews.where(cuisine: @cuisine)
+    end
 
   end
 
@@ -72,7 +83,7 @@ class ReviewsController < ApplicationController
   end
 
   def form_params
-    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance)
+    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance, :cuisine, :price)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,10 @@ class Review < ApplicationRecord
   validates :body, length: {minimum: 10}
   validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
   validates :restaurant, presence: true
-  
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
+  validates :cuisine, length: {minimum: 3}
+
+
   def to_param
     id.to_s + "-" + title.parameterize
   end
